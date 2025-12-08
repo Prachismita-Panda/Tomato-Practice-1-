0ffcf75469dacd49dc926cbc5be40d915d56714b
unknown <prachismitapanda10@gmail.com>
2025-12-08 23:04:13 +0530

    content added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div className='app'>
+    <div>
       <Navbar/>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,21 @@
-import React, { useState } from 'react'
-import './Navbar.css'
+import React from 'react'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
-  const [menu,setMenu] = useState("home");
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" />
         <ul className="navbar-menu">
-            <li className={menu==="home"?"active":""}>home</li>
-            <li className={menu==="menu"?"active":""}>menu</li>
-            <li className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li className={menu==="contact-us"?"active":""}>contact us</li>
+            <li>home</li>
+            <li>menu</li>
+            <li>mobile-app</li>
+            <li>contact us</li>
         </ul>
         <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
             <div className='navbar-search-icon'>
                 <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
             </div>
-            <button>sign in</button>
 
         </div>
       
