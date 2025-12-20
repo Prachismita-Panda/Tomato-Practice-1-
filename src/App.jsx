a8c2f23150b9b02a803dc6dd1a64f57d93701ad9
unknown <prachismitapanda10@gmail.com>
2025-12-20 21:28:14 +0530

    content added

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import Home from './components/Pages/Home/Home'
 import Cart from './components/Pages/Cart/Cart'
 import PlaceOrder from './components/Pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import { useState } from 'react'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+
+  const [ showLogin,setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           < Route path='/' element={<Home/>} />
           < Route path='/cart' element={<Cart/>} />
