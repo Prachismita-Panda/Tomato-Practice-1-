80e632e58e768ef7540b210821bc5f4ccf05d4be
unknown <prachismitapanda10@gmail.com>
2025-12-21 21:22:33 +0530

    content added

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -14,7 +14,7 @@ const LoginPopup = ({setShowLogin}) => {
             <h2>{currState}</h2>
             <img onClick={()=>setShowLogin(false)}  src={assets.cross_icon} alt="" />
         </div>
-        <div className="login-popup-input">
+        <div className="login-popup-inputs">
             {currState==="Login"?<></>: <input type="text" placeholder='Your name' required />}
             <input type="email" placeholder='Your email' required />
             <input type="password" placeholder="Password" required/>
